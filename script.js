2ab45e190f0905deee519c4f71010c584238952d
lacinajir <70260403+lacinajir@users.noreply.github.com>
2020-08-26 10:24:20 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+var entry = document.getElementById("entry");
+entry.addEventListener("click", displayDetails);
+
+var row = 1;
+
+nameCount = 0;
+
+function displayDetails() {
+	var name = document.getElementById("name").value;
+	
+	if(!name) {
+		alert("Please fill the box");
+		return;
+	}
+	
+	var display = document.getElementById("display");
+	
+	var newRow = display.insertRow(row);
+	
+	var cell1 = newRow.insertCell(0);
+
+	cell1.innerHTML = name;
+	
+	nameCount++;
+	
+	document.getElementById("insertCount").innerHTML = "Your name count: " + nameCount;
+	
+	row++;
+
+}
